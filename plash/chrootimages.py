15eca5faa5e4522662f2f526833af7ebdbd764b8
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2017-07-30 00:25:23 +0200

    update

--- plash/chrootimages.py
+++ plash/chrootimages.py
@@ -12,7 +12,7 @@ from utils import hashstr, run
 BASE_DIR = '/tmp/data'
 TMP_DIR = join(BASE_DIR, 'tmp')
 MNT_DIR = join(BASE_DIR, 'mnt')
-BUILDS_DIR = join(BASE_DIR, 'layers')
+BUILDS_DIR = join(BASE_DIR, 'builds')
 ROTATE_LOG_SIZE = 4000
 
 def log_usage(layer_name):
@@ -27,6 +27,7 @@ def staple_layer(layers, layer_cmd, rebuild=False):
     final_child_dst = join(last_layer, 'children', layer_name)
 
     if not os.path.exists(final_child_dst) or rebuild:
+        print('*** plash: building layer')
         new_child = mkdtemp(dir=TMP_DIR)
         new_layer = join(new_child, 'payload')
         os.mkdir(new_layer)
@@ -94,10 +95,23 @@ def fetch_base_image(image_name):
 
 
 def pull_base(image):
-    run(['sh', '-c', 'wget https://us.images.linuxcontainers.org/images/ubuntu/artful/amd64/default/20170729_03:49/rootfs.tar.xz | tar xf -C /tmp/bla'])
+    image_dir = join(BUILDS_DIR, image)
+    if not os.path.exists(image_dir):
+        print('*** plash downloading {}'.format(image))
+        tmpdir = mkdtemp()
+        download_file = join(tmpdir, 'rootfs.tar.xz')
+        tmp_image_dir = mkdtemp(dir=TMP_DIR) # must be on same fs than BASE_DIR for rename to work
+        os.mkdir(join(tmp_image_dir, 'children'))
+        os.mkdir(join(tmp_image_dir, 'payload'))
 
-pull_base(44)
-asdf
+        run(['wget', 'https://us.images.linuxcontainers.org/images/ubuntu/artful/amd64/default/20170729_03:49/rootfs.tar.xz', '-O', download_file])
+        run(['tar', 'xf', download_file, '--exclude=./dev', '-C', join(tmp_image_dir, 'payload')])
+        try:
+            os.rename(tmp_image_dir, image_dir)
+        except OSError as exc:
+            if exc.errno == errno.ENOTEMPTY:
+                print('*** plash: another process already pulled that image')
+
 def build(image, layers, *, quiet_flag=False, verbose_flag=False, rebuild_flag=False):
     base = [image]
     for layer in layers:
@@ -108,7 +122,7 @@ def build(image, layers, *, quiet_flag=False, verbose_flag=False, rebuild_flag=F
 def call(base, layer_commands, cmd, *, quiet_flag=False, verbose_flag=False, rebuild_flag=False, extra_mounts=[]):
     base_dir = join(BUILDS_DIR, base)
     pull_base(base)
-    layers = build(base_dir, layer_commands, rebuild_flag=True)
+    layers = build(base_dir, layer_commands, rebuild_flag=rebuild_flag)
     mountpoint = mount([join(i, 'payload') for i in layers], mkdtemp(dir=TMP_DIR))
     log_usage(layers[-1].split('/')[-1])
     os.chroot(mountpoint)
@@ -120,4 +134,4 @@ if __name__ == '__main__':
     # print(staple_layer(['/tmp/data/layers/ubuntu'], 'touch a'))
     # print(staple_layer(staple_layer(['/tmp/data/layers/ubuntu'], 'touch a'), 'touch b'))
     # print(build('/tmp/data/layers/ubuntu', ['touch a', 'touch b', 'rm a']))
-    call('ubuntu', ['touch a', 'touch b', 'rm /a'], ['/bin/bash'], rebuild_flag=True,)
+    call('ubuntu', ['touch a', 'touch b', 'rm /a'], ['/bin/bash'], rebuild_flag=False,)
