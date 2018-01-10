c85b014c2bc5c4ef2008546c476d1022961424e2
Irae Hueck Costa <mail@irae.me>
2018-01-10 12:32:05 +0000

    really nice stuff

--- plashlib/utils.py
+++ plashlib/utils.py
@@ -95,3 +95,30 @@ def handle_help_flag():
                 if do_print:
                     print(line[2:], end='')
         sys.exit(0)
+
+def filter_positionals(args):
+    positional = []
+    filtered_args = []
+    found_first_opt = False
+    while args:
+        arg = args.pop(0)
+        if not arg.startswith('-') and not found_first_opt:
+            positional.append(arg)
+        elif arg == '--':
+            positional += args
+            args = None
+        else:
+            filtered_args.append(arg)
+            found_first_opt = True
+    return positional, filtered_args
+
+def handle_build_args():
+    import subprocess
+    if len(sys.argv) >= 2 and sys.argv[1].startswith('-'):
+        cmd, args = filter_positionals(sys.argv[1:])
+        try:
+            out = subprocess.check_output(['plash-build'] + args)
+        except subprocess.CalledProcessError as exc:
+            sys.exit(exc.returncode)
+        container_id = out[:-1]
+        os.execvpe(sys.argv[0], [sys.argv[0], container_id] + cmd, os.environ)
