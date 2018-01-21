4a89707053e9e9425a0c7e26f5c3c3658a50bdbc
Irae Hueck Costa <mail@irae.me>
2018-01-21 23:58:04 +0000

    missing file

---
+++ plashlib/ux.py
@@ -0,0 +1,17 @@
+from plashlib.utils import die, nodepath_or_die
+from os.path import join
+import os
+
+PLASH_DATA = os.environ.get('PLASH_DATA', '/var/lib/plash')
+
+def assert_initialized():
+    last_inited = join(PLASH_DATA, 'index', '0')
+    if not os.path.exists(last_inited):
+        die('run plash-init first')
+
+def assert_is_root():
+    if not os.getuid() == 0:
+        die('you are not root')
+
+def assert_container_exists(cont):
+    nodepath_or_die(cont)
