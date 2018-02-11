5cb91f7ea804f018176e04071ad50caf0f9ba39c
Irae Hueck Costa <mail@irae.me>
2018-02-11 17:43:42 +0000

    short and good as pof

--- bin/plash-run-suid.go
+++ bin/plash-run-suid.go
@@ -1,6 +1,6 @@
 package main
 
-//import "syscall"
+import "syscall"
 import "bytes"
 import "os"
 import "io/ioutil"
@@ -11,59 +11,87 @@ import "path/filepath"
 import "path"
 
 func isint(val string) bool {
-    if _, err := strconv.Atoi(val); err == nil {
-        return true
-    }
-    return false
+	if _, err := strconv.Atoi(val); err == nil {
+		return true
+	}
+	return false
 
 }
 
 func main() {
-    container := os.Args[1]
-    mountpoint, err := ioutil.TempDir("/var/run", "plash-run-suid-")
-    if err != nil {panic(err)}
-    if ! isint(container) {
-        panic("argument must be an container, which is an integer")
-    }
-    link, err := os.Readlink("/var/lib/plash/index/"+container)
+	container := os.Args[1]
+	mountpoint, err := ioutil.TempDir("/var/run", "plash-run-suid-")
+	if err != nil {
+		panic(err)
+	}
+	if !isint(container) {
+		panic("argument must be an container, which is an integer")
+	}
+	link, err := os.Readlink("/var/lib/plash/index/" + container)
 
-    var buffer bytes.Buffer
-    buffer.WriteString("lowerdir=")
-    first := true
-    for {
-        cont := filepath.Base(link)
-        link = filepath.Dir(link)
-        if ! isint(cont){
-            break
-        }
-        if ! first {
-            buffer.WriteString(":")
-        }
-        first = false
-        buffer.WriteString("/var/lib/plash/index/")
-        buffer.WriteString(cont)
-        buffer.WriteString("/_data")
-    }
-    buffer.WriteString(",nosuid")
+	var buffer bytes.Buffer
+	buffer.WriteString("lowerdir=")
+	first := true
+	for {
+		cont := filepath.Base(link)
+		link = filepath.Dir(link)
+		if !isint(cont) {
+			break
+		}
+		if !first {
+			buffer.WriteString(":")
+		}
+		first = false
+		buffer.WriteString("/var/lib/plash/index/")
+		buffer.WriteString(cont)
+		buffer.WriteString("/_data")
+	}
+	buffer.WriteString(",nosuid")
 
-    err = exec.Command("mount", "-t", "overlay", "overlay", "-o", buffer.String(), mountpoint).Run()
-    if err != nil {panic(err)}
+	err = exec.Command("mount", "-t", "overlay", "overlay", "-o", buffer.String(), mountpoint).Run()
+	if err != nil {
+		panic(err)
+	}
 
-    err = exec.Command("mount", "-t", "proc", "-o", "rw,nosuid,nodev,noexec,relatime", "/proc", path.Join(mountpoint, "/proc")).Run()
-    if err != nil {panic(err)}
+	err = exec.Command("mount", "-t", "proc", "-o", "rw,nosuid,nodev,noexec,relatime", "/proc", path.Join(mountpoint, "/proc")).Run()
+	if err != nil {
+		panic(err)
+	}
 
-    err = exec.Command("mount", "-t", "none", "-o", "defaults,bind", "/home", path.Join(mountpoint, "/home")).Run()
-    if err != nil {panic(err)}
-    
-    err = exec.Command("mount", "-t", "none", "-o", "defaults,bind", "/etc/resolv.conf", path.Join(mountpoint, "/etc/resolv.conf")).Run()
-    if err != nil {panic(err)}
+	err = exec.Command("mount", "-t", "none", "-o", "defaults,bind", "/home", path.Join(mountpoint, "/home")).Run()
+	if err != nil {
+		panic(err)
+	}
+
+	err = exec.Command("mount", "-t", "none", "-o", "defaults,bind", "/etc/resolv.conf", path.Join(mountpoint, "/etc/resolv.conf")).Run()
+	if err != nil {
+		panic(err)
+	}
+
+	err = exec.Command("mount", "-t", "none", "-o", "defaults,bind", "/tmp", path.Join(mountpoint, "tmp")).Run()
+	if err != nil {
+		panic(err)
+	}
 
 
-    err = exec.Command("mount", "-t", "none", "-o", "defaults,bind", "/tmp", path.Join(mountpoint, "tmp")).Run()
-    if err != nil {panic(err)}
+        pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
 
+        err = syscall.Chroot(mountpoint)
+	if err != nil {
+		panic(err)
+	}
+        err = os.Chdir(pwd)
+	if err != nil {
+                err = os.Chdir(pwd)
+	        if err != nil {
+	        	panic(err)
+	        }
+	}
 
-    fmt.Println(mountpoint)
-
+        err = syscall.Exec("/usr/bin/env", os.Args[1:], os.Environ())
+	fmt.Println(mountpoint)
 
 }
