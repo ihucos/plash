0163d367e53a4e9f0074c6bd5fad715cfde079fa
Irae Hueck Costa <mail@irae.me>
2018-02-11 17:26:29 +0000

    well, pure go now

--- bin/plash-run-suid.go
+++ bin/plash-run-suid.go
@@ -1,25 +1,69 @@
 package main
 
-import "syscall"
+//import "syscall"
+import "bytes"
 import "os"
+import "io/ioutil"
+import "os/exec"
+import "fmt"
+import "strconv"
+import "path/filepath"
+import "path"
 
-const PrimaryBinary = "/usr/local/bin/plash-run"
-const SecondaryBinary = "/usr/bin/plash-run"
+func isint(val string) bool {
+    if _, err := strconv.Atoi(val); err == nil {
+        return true
+    }
+    return false
+
+}
 
 func main() {
-	env := os.Environ()
-	newEnv := make([]string, len(env)+1)
-	newEnv[0] = "PLASH_RUN_SUID=1"
-	for i := 1; i < len(env); i++ {
-		newEnv[i] = "_" + env[i] // "escape" environment
-	}
-	args := os.Args[:]
-	args[0] = PrimaryBinary
+    container := os.Args[1]
+    mountpoint, err := ioutil.TempDir("/var/run", "plash-run-suid-")
+    if err != nil {panic(err)}
+    if ! isint(container) {
+        panic("argument must be an container, which is an integer")
+    }
+    link, err := os.Readlink("/var/lib/plash/index/"+container)
 
-	err := syscall.Exec(PrimaryBinary, args, newEnv)
-	if err != nil && err.Error() == "no such file or directory" {
-		args[0] = SecondaryBinary
-		err = syscall.Exec(SecondaryBinary, args, newEnv)
-	}
-	panic(err)
+    var buffer bytes.Buffer
+    buffer.WriteString("lowerdir=")
+    first := true
+    for {
+        cont := filepath.Base(link)
+        link = filepath.Dir(link)
+        if ! isint(cont){
+            break
+        }
+        if ! first {
+            buffer.WriteString(":")
+        }
+        first = false
+        buffer.WriteString("/var/lib/plash/index/")
+        buffer.WriteString(cont)
+        buffer.WriteString("/_data")
+    }
+    buffer.WriteString(",nosuid")
+
+    err = exec.Command("mount", "-t", "overlay", "overlay", "-o", buffer.String(), mountpoint).Run()
+    if err != nil {panic(err)}
+
+    err = exec.Command("mount", "-t", "proc", "-o", "rw,nosuid,nodev,noexec,relatime", "/proc", path.Join(mountpoint, "/proc")).Run()
+    if err != nil {panic(err)}
+
+    err = exec.Command("mount", "-t", "none", "-o", "defaults,bind", "/home", path.Join(mountpoint, "/home")).Run()
+    if err != nil {panic(err)}
+    
+    err = exec.Command("mount", "-t", "none", "-o", "defaults,bind", "/etc/resolv.conf", path.Join(mountpoint, "/etc/resolv.conf")).Run()
+    if err != nil {panic(err)}
+
+
+    err = exec.Command("mount", "-t", "none", "-o", "defaults,bind", "/tmp", path.Join(mountpoint, "tmp")).Run()
+    if err != nil {panic(err)}
+
+
+    fmt.Println(mountpoint)
+
+
 }
