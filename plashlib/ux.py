c80189502b69a9fa46b1595cdbfe34061c93185e
Travis CI User <travis@example.org>
2018-02-23 14:26:45 +0000

    automatic prettify

--- plashlib/ux.py
+++ plashlib/ux.py
@@ -6,7 +6,7 @@ PLASH_DATA = os.environ.get('PLASH_DATA', '/var/lib/plash')
 
 
 def assert_initialized():
-    return #XXXXXXXX
+    return  #XXXXXXXX
     last_inited = join(PLASH_DATA, 'index', '0')
     if not os.path.exists(last_inited):
         die('run plash-init first')
