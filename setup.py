3fdfb1af9c82900375401ee214ec5b7da04f4795
Irae Hueck Costa <mail@irae.me>
2018-03-12 17:12:13 +0000

    PoC

--- setup.py
+++ setup.py
@@ -8,28 +8,15 @@ VERSION = '0.1dev'
 if os.environ.get('TRAVIS'):
     assert VERSION != '0.1dev', 'not building with version 0.1dev in travis'
 
-# workaround, python packaging is terrible
-if 'bdist_wheel' in sys.argv:
-    raise RuntimeError("This setup.py does not support wheels")
-
-here = path.abspath(path.dirname(__file__))
-
-bin_files = set([
-    path.join('bin', i) for i in os.listdir(path.join(here, 'bin'))
-    if not '.' in i
-])
-
-# be sure about the access rights since this is security sensitive
-for file in bin_files:
-    os.chmod(file, 0o755)
-
 setup(
     name='plash',
     version=VERSION,
     description='Container build and run tool',
     url='https://github.com/ihucos/plash',
     packages=['plashlib'],
-    data_files=[("/usr/local/bin", bin_files)],
+    scripts=['bin/plash'],
+    zip_safe=False,
+    include_package_data = True,
 
     # extra stuff
     python_requires='>=3',
