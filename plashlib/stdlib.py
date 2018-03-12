4195466999d548dfd8692af481a51d3e6142c7bc
Irae Hueck Costa <mail@irae.me>
2018-03-12 13:35:04 +0000

    update

--- plashlib/stdlib.py
+++ plashlib/stdlib.py
@@ -159,7 +159,7 @@ def namespace(ns):
 @action('f')
 @action()
 def workaround_unionfs():
-    # workaround apt so it works despite this issue: https://github.com/rpodgorny/unionfs-fuse/issues/78  
+    'workaround apt so it works despite this issue: https://github.com/rpodgorny/unionfs-fuse/issues/78'
     script = '''if [ -d /etc/apt/apt.conf.d ]; then
 echo 'Dir::Log::Terminal "/dev/null";' > /etc/apt/apt.conf.d/unionfs_workaround
 echo 'APT::Sandbox::User "root";' >> /etc/apt/apt.conf.d/unionfs_workarounds
