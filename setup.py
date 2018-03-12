86373a97078e9b4ea1632768f93721dff72671a9
Irae Hueck Costa <mail@irae.me>
2018-03-12 17:57:38 +0000

    update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='Container build and run tool',
     url='https://github.com/ihucos/plash',
     packages=['plashlib'],
-    scripts=['bin/plash'],
+    scripts=['bin/plash', 'bin/plash-exec'],
     zip_safe=False,
     include_package_data = True,
 
