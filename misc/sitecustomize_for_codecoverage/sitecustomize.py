c82400ff652cd5de8fddcfc2472b5941ea9a04fc
ihucos <irae.hueck.costa@gmail.com>
2018-03-19 20:12:15 +0100

    Travis rewrite with codecov support (#29)

---
+++ misc/sitecustomize_for_codecoverage/sitecustomize.py
@@ -0,0 +1,10 @@
+import coverage; coverage.process_startup()
+
+import os
+
+orig = os.execlp
+def patched_execlp(*args):
+    from coverage import process_startup
+    process_startup.coverage.save()
+    orig(*args)
+os.execlp = patched_execlp
