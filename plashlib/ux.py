45e27929ae06d66f9ff14212380b86142b4d6f0b
Irae Hueck Costa <irae@irae.me>
2018-08-14 15:51:46 +0200

    Fixed forgotten refactoring

--- plashlib/ux.py
+++
@@ -1,8 +0,0 @@
-from plashlib.utils import die, nodepath_or_die, get_plash_data from os.path import join
-import os
-
-
-def assert_initialized():
-    last_inited = join(get_plash_data(), 'index', '0')
-    if not os.path.exists(last_inited):
-        die('run plash init first')
