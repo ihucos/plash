1919449a3fe25224bc2cfd1eb2e10d89fc0dcc29
Irae Hueck Costa <irae@irae.me>
2018-10-23 14:16:08 +0200

    asdf

--- run.c
+++ run.c
@@ -32,28 +32,9 @@ void map_id(const char *file, u_int32_t id){
         close(fd);
 }
 
-void rootfs_mount(const char *hostdir, const char *rootfs, const char *rootfsdir) {
-        char *dst;
-        if (-1 == asprintf(&dst, "%s/%s", rootfs, hostdir)) {
-                fprintf(stderr, "myprog: asprintf returned -1\n");
-                exit(1);
-        }
-        if (-1 == mount(hostdir, dst, "none", MS_MGC_VAL|MS_BIND|MS_REC, NULL))
-                err("could not mount")
-}
 
+void setup_map(uid_t uid, gid_t gid) {
 
-int main(int argc, char* argv[]) {
-        int uid = getuid();
-        int gid = getgid();
-
-	if (-1 == unshare(CLONE_NEWNS | CLONE_NEWUSER))
-            err("could not unshare")
-
-
-        //
-        // echo deny > /proc/self/setgroups
-        //
 	FILE *fd = fopen("/proc/self/setgroups", "w");
 	if (fd < 0) {
 		if (errno != ENOENT) 
@@ -67,6 +48,25 @@ int main(int argc, char* argv[]) {
 
         map_id("/proc/self/uid_map", uid);
         map_id("/proc/self/gid_map", gid);
+}
+
+void rootfs_mount(const char *hostdir, const char *rootfs, const char *rootfsdir) {
+        char *dst;
+        struct stat sb;
+
+        if (! (stat(hostdir, &sb) == 0 && S_ISDIR(sb.st_mode)))
+                return;
+
+        if (-1 == asprintf(&dst, "%s/%s", rootfs, hostdir)) {
+                fprintf(stderr, "myprog: asprintf returned -1\n");
+                exit(1);
+        }
+        if (-1 == mount(hostdir, dst, "none", MS_MGC_VAL|MS_BIND|MS_REC, NULL))
+                err("could not rbind mount %s -> %s", hostdir, dst)
+}
+
+
+int main(int argc, char* argv[]) {
 
         if (argc < 3){
                 fprintf(stderr, "bad usage\n");
@@ -74,7 +74,21 @@ int main(int argc, char* argv[]) {
         }
 
 
+        int uid = getuid();
+        int gid = getgid();
+
+        if (uid == 0) {
+	        if (-1 == unshare(CLONE_NEWNS))
+                        err("could not unshare")
+        } else {
+	        if (-1 == unshare(CLONE_NEWNS | CLONE_NEWUSER))
+                        err("could not unshare")
+
+                setup_map(uid, gid);
+        }
+
         rootfs_mount("/tmp", argv[1], "/tmp");
+        rootfs_mount("/tmpX", argv[1], "/tmp");
 
         if (-1 == chroot(argv[1]))
                 err("could not chroot to %s", argv[1]);
