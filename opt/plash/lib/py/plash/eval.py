263c435aadd70d0070513882b2aedfc3e08f896c
Irae Hueck Costa <mail@irae.me>
2019-02-24 23:04:55 +0000

    Add opt/plash/lib

---
+++ opt/plash/lib/py/plash/eval.py
@@ -0,0 +1,132 @@
+import importlib
+import os
+import re
+import shlex
+import sys
+from functools import wraps
+
+FIND_HIND_HINT_VALUES_RE = re.compile('### plash hint: ([^=\n]+)=([^\n]+)\n')
+
+state = {'macros': {}}
+
+
+class MacroNotFoundError(Exception):
+    pass
+
+
+class MacroError(Exception):
+    def __str__(self):
+        func, macro_name, (type, value, traceback) = self.args
+        return '{macro_module}: {macro_name}: {value} ({type})'.format(
+            macro_module=func.__module__,
+            macro_name=macro_name,
+            value=value,
+            type=type.__name__)
+
+
+class EvalError(Exception):
+    pass
+
+
+def get_macros():
+    return state['macros']
+
+
+def register_macro(name=None, group='main'):
+    def decorator(func):
+        macro = name or func.__name__.replace('_', '-')
+        state['macros'][macro] = func
+        return func
+
+    return decorator
+
+
+def shell_escape_args(func):
+    @wraps(func)
+    def function_wrapper(*args):
+        args = [shlex.quote(i) for i in args]
+        return func(*args)
+
+    return function_wrapper
+
+
+def join_result(func):
+    @wraps(func)
+    def function_wrapper(*args):
+        res = func(*args)
+        return '\n'.join(res)
+
+    return function_wrapper
+
+
+def eval(lisp):
+    '''
+    plash lisp is one dimensional lisp.
+    '''
+    macro_values = []
+    if not isinstance(lisp, list):
+        raise EvalError('eval root element must be a list')
+    for item in lisp:
+        if not isinstance(item, list):
+            raise EvalError('must evaluate list of list')
+        if not all(isinstance(i, str) for i in item):
+            raise EvalError(
+                'must evaluate list of list of strings. not a list of strings: {}'
+                .format(item))
+        name = item[0]
+        args = item[1:]
+        try:
+            macro = state['macros'][name]
+        except KeyError:
+            raise MacroNotFoundError("macro {} not found".format(repr(name)))
+        try:
+            res = macro(*args)
+        except Exception as exc:
+            if os.getenv('PLASH_DEBUG', '').lower() in ('1', 'yes', 'true'):
+                raise
+            if isinstance(exc, MacroError):
+                # only raise that one time and don't have multiple wrapped MacroError
+                raise
+            raise MacroError(macro, name, sys.exc_info())
+        if not isinstance(res, str) and res is not None:
+            raise EvalError(
+                'eval macro must return string or None ({} returned {})'.
+                format(name, type(res)))
+        if res is not None:
+            macro_values.append(res)
+    return '\n'.join(macro_values)
+
+
+def remove_hint_values(script):
+    return FIND_HIND_HINT_VALUES_RE.sub('', script)
+
+
+def get_hint_values(script):
+    return FIND_HIND_HINT_VALUES_RE.findall(script)
+
+
+@register_macro('import')
+def import_(*modules):
+    'import macros from any python module'
+    output = []
+    for module_name in modules:
+        importlib.import_module(module_name)
+
+
+@register_macro()
+def reset_imports():
+    'unimport all imported macros but builtins'
+    state['macros'] = {
+        'import': import_,
+        'hint': hint,
+        'reset_imports': reset_imports
+    }
+
+
+@register_macro()
+def hint(name, value=None):
+    'write a hint for consumer programs'
+    if value is None:
+        return '### plash hint: {}'.format(name)
+    else:
+        return '### plash hint: {}={}'.format(name, value)
---
+++ opt/plash/lib/py/plash/macros/froms.py
@@ -0,0 +1,90 @@
+import atexit
+import subprocess
+from functools import wraps
+
+from plash import utils
+from plash.eval import hint, register_macro
+
+
+def cache_container_hint(cache_key_templ):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args):
+            cache_key = cache_key_templ.format(':'.join(args)).replace(
+                '/', '%')
+            container_id = utils.plash_map(cache_key)
+            if not container_id:
+                container_id = func(*args)
+                utils.plash_map(cache_key, container_id)
+            return hint('image', container_id)
+
+        return wrapper
+
+    return decorator
+
+
+@register_macro()
+@cache_container_hint('docker:{}')
+def from_docker(image):
+    'use image from local docker'
+    return subprocess.check_output(['plash', 'import-docker',
+                                    image]).decode().rstrip('\n')
+
+
+@register_macro()
+@cache_container_hint('lxc:{}')
+def from_lxc(image):
+    'use images from images.linuxcontainers.org'
+    return subprocess.check_output(['plash', 'import-lxc',
+                                    image]).decode().rstrip('\n')
+
+
+@register_macro()
+@cache_container_hint('url:{}')
+def from_url(url):
+    'import image from an url'
+    return subprocess.check_output(['plash', 'import-url',
+                                    url]).decode().rstrip('\n')
+
+
+@register_macro()
+def from_id(image):
+    'specify the image from an image id'
+    return hint('image', image)
+
+
+class MapDoesNotExist(Exception):
+    pass
+
+
+@register_macro()
+def from_map(map_key):
+    'use resolved map as image'
+    image_id = subprocess.check_output(['plash-map',
+                                        map_key]).decode().strip('\n')
+    if not image_id:
+        raise MapDoesNotExist('map {} not found'.format(repr(map_key)))
+    return hint('image', image_id)
+
+
+@register_macro('from')
+def from_(image):
+    'guess from where to take the image'
+    if image.isdigit():
+        return from_id(image)
+    else:
+        return from_lxc(image)
+
+
+@register_macro()
+@cache_container_hint('github:{}')
+def from_github(user_repo_pair, file='plashfile'):
+    "build and use a file (default 'plashfile') from github repo"
+    from urllib.request import urlopen
+    url = 'https://raw.githubusercontent.com/{}/master/{}'.format(
+        user_repo_pair, file)
+    with utils.catch_and_die([Exception], debug=url):
+        resp = urlopen(url)
+    plashstr = resp.read()
+    return utils.run_write_read(['plash', 'build', '--eval-stdin'],
+                                plashstr).decode().rstrip('\n')
