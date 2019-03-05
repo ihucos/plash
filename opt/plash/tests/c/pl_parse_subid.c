c1adfaf2aeffec93622bbd3c241c4be5eed41113
Irae Hueck Costa <mail@irae.me>
2019-03-05 18:29:53 +0100

    -

--- opt/plash/tests/c/pl_parse_subid.c
+++ opt/plash/tests/c/pl_parse_subid.c
@@ -1,42 +1,45 @@
 #include <assert.h>
-#include <plash.c>
+#include <fcntl.h>
 
-#define TEST_FILE "/tmp/plashtest"
+#include <plash.c>
 
 int main(){
 
+        char *from, *to;
+        int fd;
         // disable buffering on stdout so we get all messages
         setvbuf(stdout, NULL, _IONBF, 0);
-        char *from;
-        char *to;
+        char test_file[] = "/tmp/plashtmp.XXXXXX";
+        assert((fd = mkstemp(test_file)) != -1);
+        close(fd);
 
         puts("+ : simple subuid parse");
-        pl_printf_to_file(TEST_FILE,
+        pl_printf_to_file(test_file,
         "user1:42:2\n");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user1", "99", &from, &to) == 1);
+        assert(pl_parse_subid(test_file, "user1", "99", &from, &to) == 1);
         assert(strcmp(from, "42") == 0);
         assert(strcmp(to, "2") == 0);
 
         puts("+ : simple subuid parse find by query2");
-        pl_printf_to_file(TEST_FILE,
+        pl_printf_to_file(test_file,
         "99:42:2\n");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user", "99", &from, &to) == 1);
+        assert(pl_parse_subid(test_file, "user", "99", &from, &to) == 1);
         assert(strcmp(from, "42") == 0);
         assert(strcmp(to, "2") == 0);
 
         puts("+ : subuid not found");
-        pl_printf_to_file(TEST_FILE,
+        pl_printf_to_file(test_file,
         "user1:42:2\n");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "userXX", "99", &from, &to) == 0);
+        assert(pl_parse_subid(test_file, "userXX", "99", &from, &to) == 0);
 
         puts("+ : multiple entries");
         from = to = NULL;
-        pl_printf_to_file(TEST_FILE,
+        pl_printf_to_file(test_file,
         "user2:100:10\nuser1:42:2\n");
-        assert(pl_parse_subid(TEST_FILE, "user1", "99", &from, &to) == 1);
+        assert(pl_parse_subid(test_file, "user1", "99", &from, &to) == 1);
         assert(strcmp(from, "42") == 0);
         assert(strcmp(to, "2") == 0);
 
@@ -45,57 +48,57 @@ int main(){
         assert(pl_parse_subid("/doesnotexists", "user1", "99", &from, &to) == 0);
 
         puts("+ : empty file");
-        pl_printf_to_file(TEST_FILE, "");
+        pl_printf_to_file(test_file, "");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user1", "99", &from, &to) == 0);
+        assert(pl_parse_subid(test_file, "user1", "99", &from, &to) == 0);
 
         puts("+ : bad file");
-        pl_printf_to_file(TEST_FILE, "XXXX");
+        pl_printf_to_file(test_file, "XXXX");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user1", "99", &from, &to) == 0);
+        assert(pl_parse_subid(test_file, "user1", "99", &from, &to) == 0);
 
         puts("+ : another bad file");
-        pl_printf_to_file(TEST_FILE, "XXXX:XXXX\n");
+        pl_printf_to_file(test_file, "XXXX:XXXX\n");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user1", "99", &from, &to) == 0);
+        assert(pl_parse_subid(test_file, "user1", "99", &from, &to) == 0);
 
         puts("+ : multiple entries => take the first one");
-        pl_printf_to_file(TEST_FILE,
+        pl_printf_to_file(test_file,
         "user1:1:2\nuser1:3:4");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user1", "99", &from, &to) == 1);
+        assert(pl_parse_subid(test_file, "user1", "99", &from, &to) == 1);
         assert(strcmp(from, "1") == 0);
         assert(strcmp(to, "2") == 0);
 
         puts("+ : no newline in end of file");
-        pl_printf_to_file(TEST_FILE,
+        pl_printf_to_file(test_file,
         "user1:1:2");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user1", "99", &from, &to) == 1);
+        assert(pl_parse_subid(test_file, "user1", "99", &from, &to) == 1);
         assert(strcmp(from, "1") == 0);
         assert(strcmp(to, "2") == 0);
 
         puts("+ : no newline in end of file, big ids");
-        pl_printf_to_file(TEST_FILE,
+        pl_printf_to_file(test_file,
         "user1:1000:2000");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user1", "99", &from, &to) == 1);
+        assert(pl_parse_subid(test_file, "user1", "99", &from, &to) == 1);
         assert(strcmp(from, "1000") == 0);
         assert(strcmp(to, "2000") == 0);
 
         puts("+ : no newline, multiple entries, first match");
-        pl_printf_to_file(TEST_FILE,
+        pl_printf_to_file(test_file,
         "user1:1:2\nuser2:3:4");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user1", "99", &from, &to) == 1);
+        assert(pl_parse_subid(test_file, "user1", "99", &from, &to) == 1);
         assert(strcmp(from, "1") == 0);
         assert(strcmp(to, "2") == 0);
 
         puts("+ : no newline, multiple entries, last match");
-        pl_printf_to_file(TEST_FILE,
+        pl_printf_to_file(test_file,
         "user1:1:2\nuser2:3:4");
         from = to = NULL;
-        assert(pl_parse_subid(TEST_FILE, "user2", "99", &from, &to) == 1);
+        assert(pl_parse_subid(test_file, "user2", "99", &from, &to) == 1);
         assert(strcmp(from, "3") == 0);
         assert(strcmp(to, "4") == 0);
 
