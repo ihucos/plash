97f9f3d291de788263d31d70004d698300527c13
Irae Hueck Costa <mail@irae.me>
2019-08-25 13:34:35 +0200

    -

---
+++ opt/plash/lib/exec/parent.c
@@ -0,0 +1,20 @@
+//
+// usage: plash parent CONTAINER
+// Prints the containers parent container.
+//
+// Example:
+// $ plash parent 89
+// 88
+
+#include <plash.h>
+#include <stdio.h>
+#include <libgen.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) pl_usage();
+
+    char *nodepath = pl_check_output((char*[]){
+        "plash", "nodepath", argv[1], NULL});
+
+    puts(basename(dirname(nodepath)));
+}
