42beac319ccbedf4eaf7a00bc0cf1f3889bc0bcd
Irae Hueck Costa <mail@irae.me>
2020-01-18 20:51:16 +0100

    -

--- setup.sh
+++ setup.sh
@@ -16,5 +16,6 @@ fi
 
 set -x
 mkdir - /opt/plash
-curl -Lf https://github.com/ihucos/plash/archive/${1:-master}.tar.gz | tar -xz --strip-components=1
+curl -Lf https://github.com/ihucos/plash/archive/${1:-master}.tar.gz | tar -xzC /opt/plash --strip-components=1
+cd /opt/plash
 make
