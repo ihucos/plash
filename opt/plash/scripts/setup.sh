b6f7afca6df6846b739ceb1d57b7d626d0eb9be0
Irae Hueck Costa <mail@irae.me>
2020-01-18 14:05:43 +0100

    -

---
+++ opt/plash/scripts/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+missing=''
+
+for prog in python3 make bash cc curl gpgv
+do
+  if ! [ -x "$(command -v $prog)" ]; then
+    missing="$missing $prog"
+  fi
+done
+
+if ! [ -z "$missing" ]; then
+  echo "error: missing requirement:$missing"
+  exit 1
+fi
+
+set -x
+curl -Lf https://github.com/ihucos/plash/archive/master.tar.gz | tar -xzC / --strip-components=1
+cd /opt/plash
+make
