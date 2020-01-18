cbab80ac60a8208279f1bab8da03f3a93dd8a7ff
Irae Hueck Costa <mail@irae.me>
2020-01-18 20:08:15 +0100

    -

---
+++ exec/data.c
@@ -0,0 +1,60 @@
+// usage: plash data [CMD1 [CMD2 ...]]
+//
+// Prints the location of the build data.
+// if arguments are given, these arguments will be execed insite the build data.
+//
+//
+// Example:
+//
+// $ plash data
+// /home/myuser/.plashdata
+//
+// $ plash data cat config/union_taste
+// unionfs-fuse
+
+#define _GNU_SOURCE
+
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <plash.h>
+
+char *get_home_dir() {
+  struct passwd *pwd;
+  char *home_env;
+  if (!(home_env = getenv("HOME"))) {
+    pwd = getpwuid(getuid());
+    if (!pwd)
+      pl_fatal("could not determine your home directory");
+    return pwd->pw_dir;
+  }
+  return home_env;
+}
+
+char *get_plash_data() {
+  char *plash_data;
+  if (!(plash_data = getenv("PLASH_DATA"))) {
+    if (asprintf(&plash_data, "%s/.plashdata", get_home_dir()) == -1)
+      pl_fatal("asprintf");
+  }
+  return plash_data;
+}
+
+int main(int argc, char *argv[]) {
+  char *plash_data = get_plash_data();
+
+  // if no arguments, print the plash data directory
+  if (argc == 1) {
+    puts(plash_data);
+    return 0;
+  }
+
+  // if arguments given, cd to plash data and exec the arguments
+  if (chdir(plash_data) == -1)
+    pl_fatal("chdir %s", plash_data);
+  argv++;
+  execvp(argv[0], argv);
+  pl_fatal("exec %s", argv[0]);
+}
---
+++ exec/run.c
@@ -0,0 +1,80 @@
+// usage: plash run CONTAINER [ CMD1 [ CMD2 ... ] ]
+//
+// Run a container. If no command is specified, the containers default root
+// shell is executed.
+//
+// The following host file systems are mapped to the container:
+// - /tmp
+// - /home
+// - /root
+// - /etc/resolv.conf
+// - /sys
+// - /dev
+// - /proc
+//
+// If you want more control about how the container is run, use `plash runopts`
+
+#include <stddef.h>
+#include <plash.h>
+
+int main(int argc, char *argv[]) {
+
+  if (argc < 2)
+    pl_usage();
+
+  char *container_id = argv[1];
+  char *changesdir = pl_call("mkdtemp");
+
+  pl_exec_add(pl_path("runopts"));
+  pl_exec_add("-c");
+  pl_exec_add(container_id);
+  pl_exec_add("-d");
+  pl_exec_add(changesdir);
+  
+  // environment variables
+  pl_exec_add("-e");
+  pl_exec_add("TERM");
+  pl_exec_add("-e");
+  pl_exec_add("DISPLAY");
+  pl_exec_add("-e");
+  pl_exec_add("HOME");
+  pl_exec_add("-e");
+  pl_exec_add("PLASH_DATA");
+  pl_exec_add("-E");
+  pl_exec_add("PLASH_EXPORT");
+  
+  // mountpoints
+  pl_exec_add("-m");
+  pl_exec_add("/tmp");
+  pl_exec_add("-m");
+  pl_exec_add("/home");
+  pl_exec_add("-m");
+  pl_exec_add("/root");
+  pl_exec_add("-m");
+  pl_exec_add("/etc/resolv.conf");
+  pl_exec_add("-m");
+  pl_exec_add("/sys");
+  pl_exec_add("-m");
+  pl_exec_add("/dev");
+  pl_exec_add("-m");
+  pl_exec_add("/proc");
+
+  // command-starts-now  marker
+  pl_exec_add("--");
+
+  if (argv[2]){ // if any command specified
+    pl_exec_add("/bin/sh");
+    pl_exec_add("-lc");
+    pl_exec_add("exec env \"$@\"");
+    pl_exec_add("--");
+  }
+
+  argv++; // chop argv[0]
+  argv++; // chop the container id
+  while (argv[0])
+    pl_exec_add((argv++)[0]);
+
+  pl_exec_add(NULL);
+  
+}
+
