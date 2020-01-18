d4ac302b4d3426a1ae15aacd1e484d2154c53c58
Irae Hueck Costa <mail@irae.me>
2020-01-18 15:23:43 +0100

    -

--- setup.sh
+++ setup.sh
@@ -3,20 +3,18 @@
 set -eu
 
 missing=''
-
 for prog in python3 make bash cc curl gpgv
 do
   if ! [ -x "$(command -v $prog)" ]; then
     missing="$missing $prog"
   fi
 done
-
 if ! [ -z "$missing" ]; then
   echo "error: missing requirement:$missing"
   exit 1
 fi
 
 set -x
-curl -Lf https://github.com/ihucos/plash/archive/master.tar.gz | tar -xzC / --strip-components=1
+curl -Lf https://github.com/ihucos/plash/archive/${1:-master}.tar.gz | tar -xzC / --strip-components=1
 cd /opt/plash
 make
