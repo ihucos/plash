3ba80e4db30149d756de1800a2634fc20a8eddc7
Irae Hueck Costa <mail@irae.me>
2023-01-04 05:29:35 +0000

    .

--- exec/eval.c
+++ exec/eval.c
@@ -13,27 +13,10 @@
 #define QUOTE_REPLACE "'\"'\"'"
 
 #define current (*tokens)
-#define eachargs while (isvalorprev(*(++tokens)))
-#define commandsbegin                                                          \
-  int main(int argc, char *argv[]) {                                           \
-    tokens = argv;                                                             \
-    while (*(++tokens)) {                                                      \
-      if (0) {
-
-#define command(macro)                                                         \
-  }                                                                            \
-  else if (strcmp(current, macro) == 0) {
-
-#define commandsend                                                            \
-  }                                                                            \
-  else {                                                                       \
-    errno = 0;                                                                 \
-    pl_fatal("unknown macro: %s", current);                                    \
-  }                                                                            \
-  }                                                                            \
-  }
 
 static char **tokens;
+
+int tokenis(char *macro) { return (strcmp(current, macro) == 0); }
 
 char *quote(char *str) {
 
@@ -74,6 +57,15 @@ int isval(char *val) {
   return 1;
 }
 
+char *next_or_null() {
+  if (isval(*(tokens + 1))) {
+    tokens++;
+    return *tokens;
+  } else {
+    return NULL;
+  }
+}
+
 char *next() {
   tokens++;
   if (!isval(*tokens)) {
@@ -84,20 +76,15 @@ char *next() {
   return *tokens;
 }
 
-int isvalorprev(char *val) {
-  if (isval(val)) {
-    return 1;
-  } else {
-    tokens--;
-    return 0;
-  }
+void eachline(char *fmt) {
+  while (next_or_null())
+    printf(fmt, quote(current));
 }
-
-void eachline(char *fmt) { eachargs printf(fmt, quote(current)); }
 
 void pkg(char *cmd_prefix) {
   printf("%s", cmd_prefix);
-  eachargs printf(" %s", quote(current));
+  while (next_or_null())
+    printf(" %s", quote(current));
   printf("\n");
 }
 
@@ -127,105 +114,145 @@ char *pl_call_cached(char *subcommand, char *arg) {
   return image_id;
 }
 
-commandsbegin;
+int main(int argc, char *argv[]) {
+  tokens = argv;
+  while (*(++tokens)) {
+    if (0) {
 
-command("-x") eachargs printf("%s", current);
+    } else if (tokenis("--write-file")) {
+      char *filename = next();
+      printf("touch %s\n", quote(filename));
+      while (next_or_null())
+        printf("echo %s >> %s\n", quote(current), quote(filename));
+      while (next_or_null())
+        printf("echo %s >> %s\n", quote(current), quote(filename));
 
-command("--layer") printhint("layer", NULL);
+    } else if (tokenis("--from")) {
+      next();
+      puts("meh");
+      int i, only_digits = 1;
+      for (i = 0; current[i]; i++) {
+        if (!isdigit(current[i]))
+          only_digits = 0;
+      }
+      if (only_digits) {
+        execvp(argv[0], (char *[]){argv[0], "--from-id", current, NULL});
+      } else {
+        execvp(argv[0], (char *[]){argv[0], "--from-lxc", current, NULL});
+      }
+      puts(argv[0]);
+      pl_fatal("execvp");
 
-command("--write-file") {
-  char *filename = next();
-  printf("touch %s\n", quote(filename));
-  eachargs printf("echo %s >> %s\n", quote(current), quote(filename));
-}
-command("--env") eachline("echo %s >> /.plashenvs\n");
-command("--env-prefix") eachline("echo %s >> /.plashenvsprefix\n");
-command("--from-lxc") printhint("image", pl_call_cached("import-lxc", next()));
-command("--from") {
-  next();
+    } else if (tokenis("--from-docker")) {
+      printhint("image", pl_call_cached("import-docker", next()));
 
-  puts("meh");
-  int i, only_digits = 1;
-  for (i = 0; current[i]; i++) {
-    if (!isdigit(current[i]))
-      only_digits = 0;
+    } else if (tokenis("--from-url")) {
+      printhint("image", pl_call_cached("import-url", next()));
+
+    } else if (tokenis("--from-map")) {
+      char *image_id = pl_call("map", next());
+      if (image_id[0] == '\0') {
+        pl_fatal("No such map: %s", current);
+      }
+      printhint("image", image_id);
+
+    } else if (tokenis("--entrypoint-script")) {
+      printhint("exec", "/entrypoint");
+      printf("touch /entrypoint\n");
+      printf("chmod 755 /entrypoint\n");
+      eachline("echo %s >> /entrypoint\n");
+
+    } else if (tokenis("--eval-url")) {
+      pl_pipe((char *[]){"curl", "--fail", "--no-progress-meter", next(), NULL},
+              (char *[]){"plash", "eval-plashfile", NULL});
+
+    } else if (tokenis("--eval-github")) {
+      char *url, *user_repo_pair, *file;
+      user_repo_pair = next();
+      if (strchr(user_repo_pair, '/') == NULL)
+        pl_fatal("--eval-github: user-repo-pair must include a slash (got %s)",
+                 user_repo_pair);
+      next();
+      if (!isval(current)) {
+        file = "plashfile";
+      } else {
+        file = current;
+      }
+      asprintf(&url, "https://raw.githubusercontent.com/%s/master/%s",
+               user_repo_pair, file) != -1 ||
+          pl_fatal("asprintf");
+      pl_pipe((char *[]){"curl", "--fail", "--no-progress-meter", url, NULL},
+              (char *[]){"plash", "eval-plashfile", NULL});
+
+    } else if (tokenis("--hash-path")) {
+      while (next_or_null()) {
+        printf(": hash-path ");
+        fflush(stdout);
+        pl_pipe((char *[]){"tar", "-c", current, NULL},
+                (char *[]){"sha512sum", NULL});
+      }
+
+    } else if (tokenis("--entrypoint")) {
+      printhint("exec", next());
+
+    } else if (tokenis("--env")) {
+      eachline("echo %s >> /.plashenvs\n");
+
+    } else if (tokenis("--env-prefix")) {
+      eachline("echo %s >> /.plashenvsprefix\n");
+
+    } else if (tokenis("--eval-file")) {
+      pl_run("plash", "eval-plashfile", next());
+
+    } else if (tokenis("--eval-stdin")) {
+      pl_run("plash", "eval-plashfile");
+
+    } else if (tokenis("--from-lxc")) {
+      printhint("image", pl_call_cached("import-lxc", next()));
+
+    } else if (tokenis("--from-url")) {
+      printhint("image", next());
+
+    } else if (tokenis("--import-env")) {
+      { puts(quote(next())); }
+
+    } else if (tokenis("--layer")) {
+      printhint("layer", NULL);
+
+    } else if (tokenis("--run") || tokenis("-x")) {
+      while (next_or_null())
+        printf("%s", current);
+
+    } else if (tokenis("--apk")) {
+      pkg("apk update\napk add");
+
+    } else if (tokenis("--apt")) {
+      pkg("apt-get update\napt-get install -y");
+
+    } else if (tokenis("--dnf")) {
+      pkg("dnf install -y");
+
+    } else if (tokenis("--emerge")) {
+      pkg("emerge");
+
+    } else if (tokenis("--npm")) {
+      pkg("npm install -g");
+
+    } else if (tokenis("--pacman")) {
+      pkg("pacman -Sy --noconfirm");
+
+    } else if (tokenis("--pip")) {
+      pkg("pip install");
+
+    } else if (tokenis("--pip3")) {
+      pkg("pip3 install");
+
+    } else if (tokenis("--yum")) {
+      pkg("yum install -y");
+
+    } else {
+      errno = 0;
+      pl_fatal("unknown macro: %s", current);
+    }
   }
-
-  if (only_digits) {
-    execvp(argv[0], (char *[]){argv[0], "--from-id", current, NULL});
-  } else {
-    execvp(argv[0], (char *[]){argv[0], "--from-lxc", current, NULL});
-  }
-  puts(argv[0]);
-  pl_fatal("execvp");
 }
-command("--from-docker") {
-  printhint("image", pl_call_cached("import-docker", next()));
-}
-command("--from-url") {
-  printhint("image", pl_call_cached("import-url", next()));
-}
-command("--from-map") {
-  char *image_id = pl_call("map", next());
-  if (image_id[0] == '\0') {
-    pl_fatal("No such map: %s", current);
-  }
-  printhint("image", image_id);
-}
-command("--from-url") printhint("image", next());
-command("--entrypoint") printhint("exec", next());
-
-command("--entrypoint-script") {
-  printhint("exec", "/entrypoint");
-  printf("touch /entrypoint\n");
-  printf("chmod 755 /entrypoint\n");
-  eachline("echo %s >> /entrypoint\n");
-
-  // package managers
-}
-command("--apt") pkg("apt-get update\napt-get install -y");
-command("--apk") pkg("apk update\napk add");
-command("--yum") pkg("yum install -y");
-command("--dnf") pkg("dnf install -y");
-command("--pip") pkg("pip install");
-command("--pip3") pkg("pip3 install");
-command("--npm") pkg("npm install -g");
-command("--pacman") pkg("pacman -Sy --noconfirm");
-command("--emerge") pkg("emerge");
-command("--eval-url") {
-  pl_pipe((char *[]){"curl", "--fail", "--no-progress-meter", next(), NULL},
-          (char *[]){"plash", "eval-plashfile", NULL});
-}
-command("--eval-file") pl_run("plash", "eval-plashfile", next());
-
-command("--eval-stdin") pl_run("plash", "eval-plashfile");
-command("--eval-github") {
-  char *url, *user_repo_pair, *file;
-  user_repo_pair = next();
-  if (strchr(user_repo_pair, '/') == NULL)
-    pl_fatal("--eval-github: user-repo-pair must include a slash (got %s)",
-             user_repo_pair);
-  next();
-  if (!isval(current)) {
-    file = "plashfile";
-  } else {
-    file = current;
-  }
-  asprintf(&url, "https://raw.githubusercontent.com/%s/master/%s",
-           user_repo_pair, file) != -1 ||
-      pl_fatal("asprintf");
-  pl_pipe((char *[]){"curl", "--fail", "--no-progress-meter", url, NULL},
-          (char *[]){"plash", "eval-plashfile", NULL});
-}
-command("--hash-path") {
-  eachargs {
-    printf(": hash-path ");
-    fflush(stdout);
-    pl_pipe((char *[]){"tar", "-c", current, NULL},
-            (char *[]){"sha512sum", NULL});
-    sleep(1);
-  }
-}
-command("--import-env") { puts(quote(next())); }
-
-commandsend
