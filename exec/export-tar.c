f081041c66873f1f4b9306b0a0f0cdb31572b839
Irae Hueck Costa <mail@irae.me>
2023-01-07 00:57:07 +0000

    Port export-tar to C

---
+++ exec/export-tar.c
@@ -0,0 +1,21 @@
+// usage: plash export-tar CONTAINER [ FILE | - ]
+//
+// Export container as tar archive. It exports the file system of a container
+// to the given file as a compressed tar archive.  If no file is supplied or the
+// file is '-' the tar archive wil be printed to stdout instead.
+
+#include <stddef.h>
+#include <unistd.h>
+
+#include <plash.h>
+
+int main(int argc, char *argv[]) {
+  char *image_id, *file;
+  if (!(image_id = argv[1]))
+    pl_usage();
+  if (!(file = argv[2]))
+    file = "-";
+  pl_unshare_user();
+  execvp("plash", (char *[]){"plash", "with-mount", image_id, "tar", "-cf",
+                             file, ".", NULL});
+}
