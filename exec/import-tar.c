40a36ee9c4c587318dfee7051a52ec6e02818900
Irae Hueck Costa <mail@irae.me>
2023-01-07 00:21:40 +0000

    Port import-tar to C

---
+++ exec/import-tar.c
@@ -0,0 +1,19 @@
+// usage: plash import-tar [ TARFILE ]
+// Create a container from a tar file.
+// If the TARFILE argument is ommited, the tar file is read from stdin.
+
+#include <plash.h>
+#include <stddef.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+  char *tarfile = argv[1];
+  if (tarfile == NULL) {
+    tarfile = "-";
+  }
+  char *tmpdir = pl_call("mkdtemp");
+  pl_run("plash", "sudo", "tar", "-C", tmpdir, "-xf", tarfile);
+  execvp("plash", (char*[]){"plash", "add-layer", "0", tmpdir});
+  pl_fatal("execvp");
+}
