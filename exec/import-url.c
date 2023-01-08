4b305a25711033eb121cd070cd9dd8ac69d19406
Irae Hueck Costa <mail@irae.me>
2023-01-08 05:45:17 +0000

    Port import-url to C

---
+++ exec/import-url.c
@@ -0,0 +1,27 @@
+// usage: plash import-url URL
+// Import a container from an url.
+
+#define _GNU_SOURCE
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <plash.h>
+
+int main(int argc, char *argv[]) {
+  char *url = argv[1];
+  if (url == NULL) pl_usage();
+
+  char *rootfs = NULL;
+  asprintf(&rootfs, "%s/rootfs", pl_call("mkdtemp")) != -1 || pl_fatal("asprintf");
+  pl_run(
+      "curl",
+      "--progress-bar",
+      "--fail",
+      "--location",
+      "--output",
+      rootfs,
+      url
+      );
+  execvp("plash", (char *[]){"plash", "import-tar", rootfs, NULL});
+}
