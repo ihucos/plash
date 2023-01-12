baceca85afbb9e5ccc43aa3ab4f5e8a2358adaaa
Irae Hueck Costa <mail@irae.me>
2023-01-12 19:46:51 +0000

    Port plash purge into C

---
+++ exec/purge.c
@@ -0,0 +1,45 @@
+// usage: plash purge [ --yes ]
+// Deletes all build data unatomically. Running containers that rely on the
+// build data will enter an undefined state. Plashs behaviour while this command
+// did not finish running returning an non zero exit code is undefined. The
+// data directory itself is not deleted as it could be a mountpoint.
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <plash.h>
+
+int confirm_via_input() {
+  printf("Delete all build data? [y/N] ");
+  char inp[sizeof("y\n")];
+  fgets(inp, sizeof(inp), stdin);
+  return (strcmp(inp, "y\n") == 0);
+}
+
+int is_confirmed_via_argv(char **argv) {
+  return (!(argv[0] && (strcmp(argv[0], "--yes") == 0)));
+}
+
+int main(int argc, char *argv[]) {
+
+  if (!(is_confirmed_via_argv(argv) || confirm_via_input())) {
+    printf("Action not confirmed\n");
+    return 1;
+  }
+
+  pl_unshare_user();
+
+  char *plash_data = pl_call("data");
+  if (chdir(plash_data) == -1)
+    pl_fatal("chdir %s", plash_data);
+
+  // this one first
+  pl_run("rm", "rm", "-rf", "id_counter", NULL);
+
+  pl_run("rm", "-rf", "index");
+  pl_run("rm", "-rf", "layer");
+  pl_run("rm", "-rf", "map");
+  pl_run("rm", "-rf", "mnt");
+  pl_run("rm", "-rf", "tmp");
+}
