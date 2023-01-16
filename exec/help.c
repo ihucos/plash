bac538e30cf92efdfd631c2c0b270b45a0c1c4e3
Irae Hueck Costa <mail@irae.me>
2023-01-16 21:22:45 +0000

    Port plash help to C

---
+++ exec/help.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+
+#define HELP                                                                   \
+  "plash --help        Alias for `plash help`\n"                               \
+  "plash --help-macros Alias for `plash help-macros`\n"                        \
+  "plash --version     Alias for `plash version`\n"                            \
+  "plash -h            Alias for `plash help`\n"                               \
+  "plash add-layer     Stack a layer on top of a container\n"                  \
+  "plash b             Build then run utility\n"                               \
+  "plash build         Builds an image\n"                                      \
+  "plash clean         Cleans up plashs internal data\n"                       \
+  "plash copy          Copy the container's root filesystem to directory\n"    \
+  "plash create        Creates a new container from a command\n"               \
+  "plash data          Prints the location of the build data\n"                \
+  "plash eval          Generates a build script\n"                             \
+  "plash export-tar    Export container as tar archive\n"                      \
+  "plash help          Prints help\n"                                          \
+  "plash help-macros   Lists all available macros\n"                           \
+  "plash import-docker Import image from local docker instance into\n"         \
+  "plash import-lxc    Import an image from https://images\n"                  \
+  "plash import-tar    Create a container from a tar file\n"                   \
+  "plash import-url    Import a container from an url\n"                       \
+  "plash init          Initialize build data\n"                                \
+  "plash map           Map a container to a key\n"                             \
+  "plash mkdtemp       Create a temporary directory in the plash data\n"       \
+  "plash mount         Mount a container-filesystem\n"                         \
+  "plash mount.c       Mount a container-filesystem\n"                         \
+  "plash nodepath      Prints the path to a given container\n"                 \
+  "plash parent        Prints the containers parent container\n"               \
+  "plash purge         Deletes all build data unatomically\n"                  \
+  "plash rm            Deletes the given image atomically\n"                   \
+  "plash run           Run a container\n"                                      \
+  "plash runb          Run an image in the build environment\n"                \
+  "plash shallow-copy  Copy and hard link the filesystem to OUT\n"             \
+  "plash shrink        Delete half of the older containers\n"                  \
+  "plash sudo          Setup a Linux user namespace\n"                         \
+  "plash test          Run unit tests\n"                                       \
+  "plash version       Prints the version number\n"                            \
+  "plash with-mount    Execute parameters inside a mounted container\n"        \
+  "plash -*            Fallback to `plash b run`\n"
+
+int main(int argc, char *argv[]) { fputs(HELP, stderr); }
